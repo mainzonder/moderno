2a69bdb7c4740056596b65c6c5a24affb774bf2a
mainzonder <no7au53r@gmail.com>
2020-11-02 21:16:06 +0200

    added header

--- gulpfile.js
+++ gulpfile.js
@@ -1,68 +1,83 @@
-let gulp = require("gulp"),
-	sass = require("gulp-sass");
-rename = require("gulp-rename");
-browserSync = require("browser-sync").create();
-autoprefixer = require("gulp-autoprefixer");
-concat = require("gulp-concat");
-uglify = require("gulp-uglify");
-cssmin = require("gulp-cssmin");
-gulp.task("sass", function () {
-	return gulp
-		.src("app/scss/style.scss")
-		.pipe(sass({ outputStyle: "expanded" }))
-		.pipe(rename({ suffix: ".min" }))
-		.pipe(
-			autoprefixer({
-				overrideBrowserslist: ["last 8 versions"],
-			})
-		)
-		.pipe(gulp.dest("app/css"))
-		.pipe(browserSync.reload({ stream: true }));
+let gulp = require('gulp'),
+  sass = require('gulp-sass');
+rename = require('gulp-rename');
+browserSync = require('browser-sync').create();
+autoprefixer = require('gulp-autoprefixer');
+concat = require('gulp-concat');
+uglify = require('gulp-uglify');
+cssmin = require('gulp-cssmin');
+shorthand = require('gulp-shorthand');
+postcss = require('gulp-postcss');
+sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('sass', function () {
+  return gulp
+    .src('app/scss/**/*.scss')
+    .pipe(sass({ outputStyle: 'expanded' }))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(
+      autoprefixer({
+        overrideBrowserslist: ['last 8 versions'],
+      })
+    )
+    .pipe(shorthand())
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({ stream: true }));
 });
 
-gulp.task("html", function () {
-	return gulp.src("app/*.html").pipe(browserSync.reload({ stream: true }));
+gulp.task('css', function () {
+  return gulp
+    .src('app/css/**/style.min.css')
+    .pipe(sourcemaps.init())
+    .pipe(postcss([require('precss'), require('autoprefixer')]))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('build/'));
 });
 
-gulp.task("js", function () {
-	return gulp.src("app/js/*.js").pipe(browserSync.reload({ stream: true }));
+gulp.task('html', function () {
+  return gulp.src('app/*.html').pipe(browserSync.reload({ stream: true }));
 });
 
-gulp.task("browser-sync", function () {
-	browserSync.init({
-		server: {
-			baseDir: "app/",
-		},
-	});
+gulp.task('js', function () {
+  return gulp.src('app/js/*.js').pipe(browserSync.reload({ stream: true }));
 });
 
-gulp.task("style", function () {
-	return gulp
-		.src([
-			"node_modules/normalize.css/normalize.css",
-			"node_modules/slick-carousel/slick/slick.css",
-			"node_modules/magnific-popup/dist/magnific-popup.css",
-		])
-		.pipe(concat("libs.min.css"))
-		.pipe(cssmin())
-		.pipe(gulp.dest("app/css"));
+gulp.task('browser-sync', function () {
+  browserSync.init({
+    server: {
+      baseDir: 'app/',
+    },
+  });
 });
 
-gulp.task("script", function () {
-	return gulp
-		.src([
-			"node_modules/slick-carousel/slick/slick.js",
-			"node_modules/magnific-popup/dist/jquery.magnific-popup.js",
-		])
-		.pipe(concat("libs.min.js"))
-		.pipe(uglify())
-		.pipe(gulp.dest("app/js"));
+gulp.task('style', function () {
+  return gulp
+    .src([
+      'node_modules/normalize.css/normalize.css',
+      'node_modules/slick-carousel/slick/slick.css',
+      'node_modules/magnific-popup/dist/magnific-popup.css',
+    ])
+    .pipe(concat('libs.min.css'))
+    .pipe(cssmin())
+    .pipe(gulp.dest('app/css'));
 });
 
-gulp.task("watch", function () {
-	gulp.watch("app/scss/style.scss", gulp.parallel("sass"));
-	gulp.watch("app/*.html", gulp.parallel("html"));
-	gulp.watch("app/js/*.js", gulp.parallel("js"));
+gulp.task('script', function () {
+  return gulp
+    .src([
+      'node_modules/slick-carousel/slick/slick.js',
+      'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
+    ])
+    .pipe(concat('libs.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('app/js'));
 });
 
-gulp.task("default", gulp.parallel("script", "sass", "watch", "browser-sync"));
+gulp.task('watch', function () {
+  gulp.watch('app/scss/**/*.scss', gulp.parallel('sass'));
+  gulp.watch('app/css/style.min.css', gulp.parallel('css'));
+  gulp.watch('app/*.html', gulp.parallel('html'));
+  gulp.watch('app/js/*.js', gulp.parallel('js'));
+});
+
+gulp.task('default', gulp.parallel('style', 'script', 'sass', 'watch', 'browser-sync', 'css'));
