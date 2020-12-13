73a7a61060221b891f7341b966a11bfdb3a23348
mainzonder <no7au53r@gmail.com>
2020-12-13 08:09:58 +0200

    added page.html

--- app/js/main.js
+++ app/js/main.js
@@ -1,16 +1,23 @@
 $(function () {
-    $('.rate-star').rateYo({
-        rating: 5,
-        starWidth: "12px",
-        readOnly:true
-    });
-    $('.product-slider__inner').slick({
-        dots: true,
-        arrows: false,
-        slidesToShow: 4,
-        slidesToScroll: 4
-    });
+	$('.rate-star').rateYo({
+		rating: 5,
+		starWidth: '12px',
+		readOnly: true,
+	});
+	$('.product-slider__inner').slick({
+		dots: true,
+		arrows: false,
+		slidesToShow: 4,
+		slidesToScroll: 4,
+	});
 
+	$('.menu__btn').on('click', function () {
+		$('.menu__list').slideToggle();
+	});
 
-    var mixer = mixitup('.products__inner-box');
+	$('.header__btn-menu').on('click', function () {
+		$('.header__box').toggleClass('active');
+	});
+
+	var mixer = mixitup('.products__inner-box');
 });
